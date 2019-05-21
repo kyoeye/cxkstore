7bd11e676b6e358302db0ec7dae5e8d786801cbc
Hasee <Hasee@DESKTOP-C45O6SE>
2019-05-21 11:33:54 +0800

    testtest

--- cxkstore/Mainpage.aspx
+++ cxkstore/Mainpage.aspx
@@ -10,6 +10,9 @@
 </head>
 <body>
         <div>
+                    <button id="Button2"  class="button" runat="server">登录</button><br />
+                    <button id="Button3"  class="button" runat="server">登录</button><br />
+                    <button id="Button4"  class="button" runat="server">登录</button><br />
                     <button id="Button1"  class="button" runat="server">登录</button><br />
              
         </div>
--- cxkstore/Mainpage.aspx.designer.cs
+++ cxkstore/Mainpage.aspx.designer.cs
@@ -13,6 +13,33 @@ namespace cxkstore {
     public partial class Mainpage {
         
         /// <summary>
+        /// Button2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton Button2;
+        
+        /// <summary>
+        /// Button3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton Button3;
+        
+        /// <summary>
+        /// Button4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton Button4;
+        
+        /// <summary>
         /// Button1 控件。
         /// </summary>
         /// <remarks>
